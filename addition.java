d392f34c71e209eeec31c012509f97fa47d3c38f
andanayya_hiremath <anduhiremath48@gmail.com>
2023-02-12 18:48:31 +0530

    new commit

---
+++ addition.java
@@ -0,0 +1,18 @@
+/**
+ * add
+ */
+public class addition {
+public static int add() {
+    int a = 30;
+    int b = 40;
+
+    int sum = a+b;
+    return sum;
+
+}
+ public static void main(String[] args) {
+    int result = add();
+    System.out.println("the sum of the numbres is "+result);
+}
+    
+}
